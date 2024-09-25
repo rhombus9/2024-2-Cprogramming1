54bc68e6d01ee9dc3a780dcb5cb5bca98f52db5b
rhombus9 <rhombusvanced@gmail.com>
2024-09-25 10:54:58 +0900

    add file

---
+++ hw3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	for (int i = 1; i < 6; i++) {
+		for (int j = 0; j < 5-i; j++) {
+			printf(" ");
+		}
+		for (int k = 0; k < i * 2 - 1; k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+}
